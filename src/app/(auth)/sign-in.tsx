06b832cff3d199c5e896a31382aa280ce8c39d02
mfgcorpusjr <mcorpus.dev@gmail.com>
2025-07-23 21:24:10 +0800

    folder structure

---
+++ src/app/(auth)/sign-in.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function SignInScreen() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Sign In Screen</Text>
+    </View>
+  );
+}
---
+++ src/app/(auth)/sign-up.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function SignUpScreen() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Sign Up Screen</Text>
+    </View>
+  );
+}
